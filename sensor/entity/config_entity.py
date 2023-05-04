f1c4ad542fd3d3e84a22b08388aab23192b42a4f
Nandhu-12 <nandhusaravanan12@gmail.com>
2023-05-04 19:26:56 +0530

    Data Ingestion and Data Validation

--- sensor/entity/config_entity.py
+++ sensor/entity/config_entity.py
@@ -5,37 +5,42 @@ from datetime import datetime
 FILE_NAME = "sensor1.csv"
 TRAIN_FILE_NAME = "train.csv"
 TEST_FILE_NAME = "test.csv"
-
 class TrainingPipelineConfig:
-
     def __init__(self):
         try:
+            #here we store each output in artifact folder and we have timestamp folder inside artifact folder
             self.artifact_dir = os.path.join(os.getcwd(),"artifact",f"{datetime.now().strftime('%m%d%Y__%H%M%S')}")
-        except Exception  as e:
-            raise SensorException(e,sys)     
-
-
+        except Exception as e:
+            raise SensorException(e,sys)
 class DataIngestionConfig:
-
     def __init__(self,training_pipeline_config:TrainingPipelineConfig):
         try:
-            self.db_name="aps"
-            self.col_name="sensor1"
-            self.data_ingestion_dir = os.path.join(training_pipeline_config.artifact_dir , "data_ingestion")
-            self.feature_store_file_path = os.path.join(self.data_ingestion_dir,"feature_store",FILE_NAME)
+            self.db_name = "aps"
+            self.col_name = "sensor1"
+            #creating data_ingestion folder inside timestamp folder
+            self.data_ingestion_dir = os.path.join(training_pipeline_config.artifact_dir, "data_ingestion")
+            #feature store file is where the csv file gets stored
+            self.feature_store_file_path = os.path.join(self.data_ingestion_dir,"feature store",FILE_NAME)
             self.train_file_path = os.path.join(self.data_ingestion_dir,"dataset",TRAIN_FILE_NAME)
             self.test_file_path = os.path.join(self.data_ingestion_dir,"dataset",TEST_FILE_NAME)
             self.test_size = 0.2
-        except Exception  as e:
-            raise SensorException(e,sys)     
-
+        except Exception as e:
+            raise SensorException(e,sys)
     def to_dict(self,)->dict:
         try:
             return self.__dict__
-        except Exception  as e:
-            raise SensorException(e,sys)     
+        except Exception as e:
+            raise SensorException(e,sys)
 
-class DataValidationConfig:...
+class DataValidationConfig:
+
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        self.data_validation_dir = os.path.join(training_pipeline_config.artifact_dir , "data_validation")
+        self.report_file_path=os.path.join(self.data_validation_dir, "report.yaml")
+        self.missing_threshold:float = 0.2
+        self.base_file_path = os.path.join("aps_failure_training_set1.csv")
+
+
 class DataTransformationConfig:...
 class ModelTrainerConfig:...
 class ModelEvaluationConfig:...
